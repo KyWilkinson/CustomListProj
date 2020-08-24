85cc500ccfa29ddc13e8a2ce0fb516a5995d9aa2
KyWilkinson <wilki018@hotmail.com>
2020-08-24 13:46:15 -0500

    Created test unit and custom list class

---
+++ CustomListMethodTesting/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProject;
+using System.Collections.Generic;
+
+namespace CustomListMethodTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_TwoValues_CountOf2()
+        {
+
+        }
+
+    }
+}
---
+++ CustomListProject/CustomList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListProject
+{
+    public class CustomList<T>
+    {
+
+        T[] _items;
+
+        int _capacity;
+        int _count;
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+            set
+            {
+                _capacity = value;
+            }
+        }
+        public CustomList()
+        {
+            _count = 0;
+            _capacity = 4;
+            _items = new T[_capacity];
+        }
+
+
+        public void Add(T item)
+        {
+            if(_count == _capacity)
+            {
+                _items = new T[_capacity * 2];
+            }
+            else
+            {
+                _items[_count] = item;
+                _count++;
+
+            }
+
+        }
+    }
+}
