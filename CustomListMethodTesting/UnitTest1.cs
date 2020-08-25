8656f717033156cd3f1672bf9f1b438dff35e317
KyWilkinson <wilki018@hotmail.com>
2020-08-25 09:35:47 -0500

    Created three out of five tests for the Add methods

--- CustomListMethodTesting/UnitTest1.cs
+++ CustomListMethodTesting/UnitTest1.cs
@@ -11,7 +11,65 @@ namespace CustomListMethodTesting
         [TestMethod]
         public void Add_TwoValues_CountOf2()
         {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 2;
+            int value2 = 4;
+            int expected = 2;
+            int actual;
 
+            //Act
+            customList.Add(value1);
+            customList.Add(value2);
+            actual = customList.Count;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_FourValues_CountOf4()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 2;
+            int value2 = 4;
+            int value3 = 6;
+            int value4 = 10;
+            int expected = 4;
+            int actual;
+
+            //Act
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value3);
+            customList.Add(value4);
+            actual = customList.Count;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_TwoValues_CountOf10()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 2;
+            int value2 = 4;
+            int expected = 10;
+            int actual;
+
+            //Act
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value1);
+            customList.Add(value2);
+            actual = customList.Count;
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
 
     }
