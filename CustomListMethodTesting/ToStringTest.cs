45189c30cd243b5bad669c7944231fe53bc7ce3b
KyWilkinson <wilki018@hotmail.com>
2020-08-27 16:38:44 -0500

    Created test class for ToString method

---
+++ CustomListMethodTesting/ToStringTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListMethodTesting
+{
+    [TestClass]
+    public class ToStringTest
+    {
+        [TestMethod]
+        public void Display_Ints_Convert()
+        {
+        }
+    }
+}
